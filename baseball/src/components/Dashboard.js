6449c5552762055658da640c08c0fcef6559d8f8
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2019-03-06 12:56:51 -0500

    Added unit test to test if addBall function is fired

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -4,8 +4,12 @@ const Dashboard = props => {
   return (
     <div className="Dashboard">
       <h2>Baseball Dashboard</h2>
-      <button onClick={props.addStrike}>Strike</button>
-      <button onClick={props.addBall}>Ball</button>
+      <button data-testid="addStrike" onClick={props.addStrike}>
+        Strike
+      </button>
+      <button data-testid="addBall" onClick={props.addBall}>
+        Ball
+      </button>
       <button onClick={props.addFoul}>Foul</button>
       <button onClick={props.addHit}>Hit</button>
     </div>
--- baseball/src/components/Dashboard.spec.js
+++ baseball/src/components/Dashboard.spec.js
@@ -5,12 +5,19 @@ import "jest-dom/extend-expect"; // added this
 import Dashboard from "./Dashboard";
 
 describe("<Dashboard />", () => {
-  it("calls the addBall function when clicked", () => {
-    const { getByText } = render(<Dashboard />);
+  //   it("should fire addBall function when button is clicked", () => {
+  //     const onClick = jest.fn();
+  //     const { getByTestId } = render(<Dashboard addBall={onClick} />);
+  //     const add_ball = getByTestId("addBall");
+  //     fireEvent.click(add_ball);
+  //     expect(onClick).toHaveBeenCalled();
+  //   });
 
-    const addBallButton = getByText(/ball/i);
-    fireEvent.click(addBallButton);
-
-    expect(addBallButton).toHaveBeenCalled();
+  it("should fire addStrike function when button is clicked", () => {
+    const onClick = jest.fn();
+    const { getByTestId } = render(<Dashboard addStrike={onClick} />);
+    const add_strike = getByTestId("addStrike");
+    fireEvent.click(add_strike);
+    expect(onClick).toHaveBeenCalled();
   });
 });
