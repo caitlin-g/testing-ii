8e44f78817ac0536f2ba7499516ce3f8ce180b0b
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2019-03-06 12:57:11 -0500

    Added snapshot test to App.js

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,13 +1,20 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
 
-import { render, fireEvent } from "react-testing-library";
 import "jest-dom/extend-expect";
 
 import App from "./App";
 
 describe("<App />", () => {
   it("renders without crashing", () => {
-    const helpers = render(<App />);
-    // console.log("helpers", helpers);
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("matches snapshot", () => {
+    const tree = renderer.create(<App />);
+    expect(tree.toJSON()).toMatchSnapshot();
   });
 });
